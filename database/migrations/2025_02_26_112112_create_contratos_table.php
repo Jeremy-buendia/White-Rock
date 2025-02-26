9751370dfc8697d75fa1f6193adfc58f6ad68031
Jairo <jaiiro.cereceda@gmail.com>
2025-02-26 12:27:19 +0100

    fin bd

---
+++ database/migrations/2025_02_26_112112_create_contratos_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContratosTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('contratos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('propiedad_id')->constrained('propiedades')->onDelete('cascade');
+            $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
+            $table->foreignId('agente_id')->constrained('agentes_inmobiliarios')->onDelete('cascade');
+            $table->enum('tipo_contrato', ['compra', 'venta', 'alquiler']);
+            $table->date('fecha_inicio');
+            $table->date('fecha_finalizacion')->nullable();
+            $table->text('condiciones')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('contratos');
+    }
+}
