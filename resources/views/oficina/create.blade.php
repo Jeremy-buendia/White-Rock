936bbf12074df2a3495daaf430317dd5908c8497
Jairo <jaiiro.cereceda@gmail.com>
2025-03-10 22:36:21 +0100

    Cambios en oficina

--- resources/views/oficina/create.blade.php
+++ resources/views/oficina/create.blade.php
@@ -1,45 +1,49 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('oficina.crear') }}">
-        @csrf
+<x-agente-layout>
+    <div class="container-lg">
+        <br>
+        <h1 class="mt-4 mb-4">Crear Oficina</h1>
+        <form method="POST" action="{{ route('oficina.crear') }}" class="row g-3">
+            @csrf
 
-        <!-- Nombre -->
-        <div>
-            <x-input-label for="nombre" :value="__('Nombre')" />
-            <x-text-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre')" required
-                autofocus autocomplete="nombre" />
-            <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
-        </div>
+            <!-- Nombre -->
+            <div class="col-md-6">
+                <x-input-label for="nombre" class="form-label" :value="__('Nombre')" />
+                <x-text-input id="nombre" class="form-control" type="text" name="nombre" :value="old('nombre')" required
+                    autofocus autocomplete="nombre" />
+                <x-input-error :messages="$errors->get('nombre')" class="invalid-feedback d-block" />
+            </div>
 
+            <!-- Dirección -->
+            <div class="col-md-6">
+                <x-input-label for="direccion" class="form-label" :value="__('Dirección')" />
+                <x-text-input id="direccion" class="form-control" type="text" name="direccion" :value="old('direccion')"
+                    required autocomplete="direccion" />
+                <x-input-error :messages="$errors->get('direccion')" class="invalid-feedback d-block" />
+            </div>
 
-        <!-- Dirección -->
-        <div class="mt-4">
-            <x-input-label for="direccion" :value="__('Dirección')" />
-            <x-text-input id="direccion" class="block mt-1 w-full" type="text" name="direccion" :value="old('direccion')"
-                required autocomplete="direccion" />
-            <x-input-error :messages="$errors->get('direccion')" class="mt-2" />
-        </div>
+            <!-- Teléfono -->
+            <div class="col-md-6">
+                <x-input-label for="telefono" class="form-label" :value="__('Teléfono')" />
+                <x-text-input id="telefono" class="form-control" type="text" name="telefono" :value="old('telefono')"
+                    required autocomplete="telefono" />
+                <x-input-error :messages="$errors->get('telefono')" class="invalid-feedback d-block" />
+            </div>
 
-        <!-- Teléfono -->
-        <div class="mt-4">
-            <x-input-label for="telefono" :value="__('Teléfono')" />
-            <x-text-input id="telefono" class="block mt-1 w-full" type="text" name="telefono" :value="old('telefono')"
-                required autocomplete="telefono" />
-            <x-input-error :messages="$errors->get('telefono')" class="mt-2" />
-        </div>
+            <!-- Fax -->
+            <div class="col-md-6">
+                <x-input-label for="fax" class="form-label" :value="__('Fax: ')" />
+                <x-text-input id="fax" class="form-control" type="text" name="fax" :value="old('fax')"
+                    required autocomplete="fax" />
+                <x-input-error :messages="$errors->get('fax')" class="invalid-feedback d-block" />
+            </div>
 
-        <!-- Fax -->
-        <div class="mt-4">
-            <x-input-label for="fax" :value="__('Fax: ')" />
-            <x-text-input id="fax" class="block mt-1 w-full" type="text" name="fax" :value="old('fax')"
-                required autocomplete="fax" />
-            <x-input-error :messages="$errors->get('fax')" class="mt-2" />
-        </div>
-
-        <!-- Botón de Registro -->
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button class="ms-4">
-                {{ __('Crear') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+            <!-- Botón de Registro -->
+            <div class="col-12">
+                <x-primary-button class="btn btn-primary">
+                    {{ __('Crear') }}
+                </x-primary-button>
+                <a href="{{ route('agente.dashboard') }}" class="btn btn-secondary">Volver</a>
+            </div>
+        </form>
+    </div>
+</x-agente-layout>
--- resources/views/oficina/index.blade.php
+++ resources/views/oficina/index.blade.php
@@ -1,11 +1,16 @@
 <x-agente-layout>
-    <main class="">
+    <main class="container-lg">
+        <img src=" {{ asset('img/ofice.jpg') }}" alt="" class="img-ofice">
+        <h1>Datos de la oficina</h1>
         <nav>
             <ul>
-                <li> {{ $oficina->nombre }}</li>
-                <li> {{ $oficina->direccion }}</li>
-                <li> {{ $oficina->telefono }}</li>
-                <li> {{ $oficina->fax }}</li>
+                <li><b>Nombre: </b> {{ $oficina->nombre }}</li>
+                <hr>
+                <li><b>Dirección: </b> {{ $oficina->direccion }}</li>
+                <hr>
+                <li><b>Teléfono: </b> {{ $oficina->telefono }}</li>
+                <hr>
+                <li><b>Fax: </b> {{ $oficina->fax }}</li>
             </ul>
         </nav>
         <a href="{{ route('oficina.crear') }}">Crear Oficina</a>
